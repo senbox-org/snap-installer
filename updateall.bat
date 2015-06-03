b96c89d6f16dec3bccdee16a345e4f68f6e701d5
forman <norman.fomferra@brockmann-consult.de>
2015-06-03 14:56:19 +0200

    exit on error

--- updateall.bat
+++ updateall.bat
@@ -5,41 +5,49 @@ echo Git updates...
 if exist snap-engine\.git (
     cd snap-engine
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist snap-desktop\.git (
     cd snap-desktop
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist snap-examples\.git (
     cd snap-examples
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist snap-installer\.git (
     cd snap-installer
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist s1tbx\.git (
     cd s1tbx
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist s2tbx\.git (
     cd s2tbx
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist s3tbx\.git (
     cd s3tbx
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist testdata\.git (
     cd testdata
     call git pull
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 
@@ -48,34 +56,42 @@ echo Maven builds...
 if exist snap-engine\pom.xml (
     cd snap-engine
     call mvn clean install -T 4
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist snap-desktop\pom.xml (
     cd snap-desktop
     call mvn clean install -T 4 -DskipTests=true
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd snap-application
     call mvn nbm:cluster-app
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..\..
 )
 if exist snap-examples\pom.xml (
     cd snap-examples
     call mvn clean install -T 4
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist s1tbx\pom.xml (
     cd s1tbx
     call mvn clean 
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     call mvn install -T 4 -DskipTests=true
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist s2tbx\pom.xml (
     cd s2tbx
     call mvn clean install -T 4
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 if exist s3tbx\pom.xml (
     cd s3tbx
     call mvn clean install -T 4
+    if %errorlevel% gtr 0 exit /B %errorlevel%
     cd ..
 )
 
