caa3a557a443fd06c66a19581fdb19d4ac115c8d
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-10-06 15:36:09 +0200

    Escaped the '|' character

--- files/winx32/bin/snappy-conf.bat
+++ files/winx32/bin/snappy-conf.bat
@@ -10,7 +10,7 @@ goto End
 :Usage
 @echo Configures the SNAP-Python interface 'snappy'.
 @echo. 
-@echo %~n0 Python [Dir] | [/?]
+@echo %~n0 Python [Dir] ^| [/?]
 @echo.  
 @echo     Python: Full path to Python executable to be used with SNAP, e.g. C:\Python34\python.exe
 @echo     Dir:    Directory where the 'snappy' module should be installed. Defaults to %USERPROFILE%\.snap\snap-python
