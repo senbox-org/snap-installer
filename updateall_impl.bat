25437516bf09066e748a3562eafebc74aafb2dc9
Marco Peters <marco.peters@brockmann-consult.de>
2023-04-18 10:14:33 +0200

    update UpdateAll to make use of new toolboxes (removed old)

--- updateall_impl.bat
+++ updateall_impl.bat
@@ -4,16 +4,14 @@ cd %0\..\..
 
 echo Git updates...
 
-set skipTests=true
+set skipTests=false
 
 set buildSnapEngine=true
 set buildSnapDesktop=true
 set buildSnapExamples=false
-set buildS1TBX=false
-set buildS2TBX=false
-set buildS3TBX=false
-set buildSMOSBOX=false
-set buildProbavBox=false
+set buildMwvTBX=true
+set buildOptTBX=true
+set buildSMOSBOX=true
 
 if exist snap-engine\.git (
     cd snap-engine
@@ -39,32 +37,21 @@ if exist snap-installer\.git (
     if %errorlevel% neq 0 exit /B %errorlevel%
     cd ..
 )
-if exist s1tbx\.git (
-    cd s1tbx
+if exist microwave-toolbox\.git (
+    cd microwave-toolbox
     call git pull
     if %errorlevel% neq 0 exit /B %errorlevel%
     cd ..
 )
-if exist s2tbx\.git (
-    cd s2tbx
+if exist optical-toolbox\.git (
+    cd optical-toolbox
     call git pull
     if %errorlevel% neq 0 exit /B %errorlevel%
     cd ..
 )
-if exist s3tbx\.git (
-    cd s3tbx
-    call git pull
-    if %errorlevel% neq 0 exit /B %errorlevel%
-    cd ..
-)
+
 if exist smos-box\.git (
     cd smos-box
-    call git pull
-    if %errorlevel% neq 0 exit /B %errorlevel%
-    cd ..
-)
-if exist probavbox\.git (
-    cd probavbox
     call git pull
     if %errorlevel% neq 0 exit /B %errorlevel%
     cd ..
@@ -78,9 +65,11 @@ if exist testdata\.git (
 
 echo Maven builds...
 
+call mvn -v
+
 if exist snap-engine\pom.xml if %buildSnapEngine% EQU true (
     cd snap-engine
-    call mvn clean install -T 4 -DskipTests=%skipTests%
+    call mvn clean install -T 6 -DskipTests=%skipTests%
     if %errorlevel% neq 0 exit /B %errorlevel%
     call mvn nbm:autoupdate
     if %errorlevel% neq 0 exit /B %errorlevel%
@@ -88,7 +77,7 @@ if exist snap-engine\pom.xml if %buildSnapEngine% EQU true (
 )
 if exist snap-desktop\pom.xml if %buildSnapDesktop% EQU true (
     cd snap-desktop
-    call mvn clean install -T 4 -DskipTests=%skipTests%
+    call mvn clean install -T 6 -DskipTests=%skipTests%
     if %errorlevel% neq 0 exit /B %errorlevel%
     call mvn nbm:autoupdate
     if %errorlevel% neq 0 exit /B %errorlevel%
@@ -99,56 +88,35 @@ if exist snap-desktop\pom.xml if %buildSnapDesktop% EQU true (
 )
 if exist snap-examples\pom.xml if %buildSnapExamples% EQU true (
     cd snap-examples
-    call mvn clean install -T 4  -DskipTests=%skipTests%
+    call mvn clean install -T 6  -DskipTests=%skipTests%
     if %errorlevel% neq 0 exit /B %errorlevel%
     call mvn nbm:autoupdate
     if %errorlevel% neq 0 exit /B %errorlevel%
     cd ..
 )
-if exist s1tbx\pom.xml if %buildS1TBX% EQU true (
-    cd s1tbx
+if exist microwave-toolbox\pom.xml if %buildMwvTBX% EQU true (
+    cd microwave-toolbox
     call mvn clean
     if %errorlevel% neq 0 exit /B %errorlevel%
-    call mvn install -T 4 -DskipTests=%skipTests%
+    call mvn install -T 6 -DskipTests=%skipTests%
     if %errorlevel% neq 0 exit /B %errorlevel%
     call mvn nbm:autoupdate
     if %errorlevel% neq 0 exit /B %errorlevel%
     cd ..
 )
-if exist s2tbx\pom.xml if %buildS2TBX% EQU true (
-    cd s2tbx
-    call mvn clean install -T 4 -DskipTests=%skipTests%
-    if %errorlevel% neq 0 exit /B %errorlevel%
-    call mvn nbm:autoupdate
-    if %errorlevel% neq 0 exit /B %errorlevel%
-
-    cd s2tbx-sta-adapters\sen2cor
-    call mvn clean install -T 4 -DskipTests=%skipTests%
-    if %errorlevel% neq 0 exit /B %errorlevel%
-    call mvn nbm:autoupdate
-    if %errorlevel% neq 0 exit /B %errorlevel%
-
-    cd ..\..\..
-)
-if exist s3tbx\pom.xml if %buildS3TBX% EQU true (
-    cd s3tbx
-    call mvn clean install -T 4 -DskipTests=%skipTests%
+if exist optical-toolbox\pom.xml if %buildOptTBX% EQU true (
+    cd optical-toolbox
+    call mvn clean install -DskipTests=%skipTests%
     if %errorlevel% neq 0 exit /B %errorlevel%
     call mvn nbm:autoupdate
     if %errorlevel% neq 0 exit /B %errorlevel%
     cd ..
 )
+
+
 if exist smos-box\pom.xml if %buildSMOSBOX% EQU true (
     cd smos-box
-    call mvn clean install -T 4 -DskipTests=%skipTests%
-    if %errorlevel% neq 0 exit /B %errorlevel%
-    call mvn nbm:autoupdate
-    if %errorlevel% neq 0 exit /B %errorlevel%
-    cd ..
-)
-if exist probavbox\pom.xml if %buildProbavBox% EQU true (
-    cd probavbox
-    call mvn clean install -T 4 -DskipTests=%skipTests%
+    call mvn clean install -T 6 -DskipTests=%skipTests%
     if %errorlevel% neq 0 exit /B %errorlevel%
     call mvn nbm:autoupdate
     if %errorlevel% neq 0 exit /B %errorlevel%
