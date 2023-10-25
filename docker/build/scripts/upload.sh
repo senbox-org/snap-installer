349970714ce5ade56fb93d03b9d0334a7cd222ea
BALDO Frederick <frederick.baldo@csgroup.eu>
2023-10-25 10:57:50 +0200

    Update image tag

--- docker/build/scripts/upload.sh
+++ docker/build/scripts/upload.sh
@@ -7,15 +7,22 @@ export UPLOAD_URL=$4
 
 for file in $(ls ${USER_HOME_DIR});
 do 
-    if [[ "${file}" =~ ^esa-snap_all* ]]
+    # if [[ "${file}" =~ ^esa-snap_all* ]]
+    if [[ "${file}" =~ ^esa-snap* ]]
     then
+        echo $file
         export OS=$(echo "$file" | grep -oP  "(unix|windows|macos)")
-        export VERSION=$(echo "$file" | grep -oP  "(1[0-9]{1,2})(_)([0-9]*)(_)([0-9]*)(-SNAPSHOT)?")
+        export VERSION="9.x"
         export EXT=""
         export GROUP="installers"
+        export REPO="snap-maven-releases"
         if [[ "${file}" == *".dmg"* ]]
             then
                 EXT="dmg"
+                if [[ "${file}" == *"archive"* ]]
+                    then
+                        OS="macos_archive"
+                fi
         elif [[ "${file}" == *".exe"* ]]
             then
                 EXT="exe"
@@ -28,11 +35,11 @@ do
         fi
         if [[ "${file}" != "*x32*" ]]
             then
-                echo "Uploading ${file} to ${UPLOAD_URL}"
+                echo "Uploading ${file} to ${REPO}"
                 curl -u ${NEXUS_USER}:${NEXUS_PASS} --progress-bar \
                     -F "maven2.groupId=org.esa.snap.${GROUP}" -F "maven2.artifactId=snap_all_${OS}" -F "maven2.version=${VERSION}" \
                     -F "maven2.asset1=@${USER_HOME_DIR}/${file}" -F "maven2.asset1.extension=${EXT}" \
-                    "${UPLOAD_URL}"
+                    "${UPLOAD_URL}=${REPO}"
         fi
     fi
 done
