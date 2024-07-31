cbbaa9f98f1bd5d095396228ed06cdc70a796f98
BALDO Frederick <frederick.baldo@csgroup.eu>
2024-07-31 09:45:43 +0200

    feat(ci): Updated version to snap 12.0.0-SNAPSHOT

--- docker/build/scripts/upload.sh
+++ docker/build/scripts/upload.sh
@@ -12,7 +12,7 @@ do
     then
         echo $file
         export OS=$(echo "$file" | grep -oP  "(linux|windows|macos)")
-        export VERSION="11.0.0"
+        export VERSION="12.0.0"
         export EXT=""
         export GROUP="installers"
         export REPO="snap-maven-releases"
