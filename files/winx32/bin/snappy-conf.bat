8cb93a4695d9758d45dcd6edc0f9d657f8d89cd6
Olaf Danne <olaf.danne@brockmann-consult.de>
2025-02-14 18:21:17 +0100

    esa_snappy: improved logging of snappy config; snap-installer: improved config explanations in installer GUI, minor adaptations in snappy-conf scripts

--- files/winx32/bin/snappy-conf.bat
+++ files/winx32/bin/snappy-conf.bat
@@ -9,10 +9,11 @@ goto End
 
 :Usage
 @echo Configures the SNAP-Python interface 'esa_snappy'.
-@echo. 
+@echo.
+@echo Usage:
 @echo %~n0 Python [Dir] ^| [/?]
 @echo.  
 @echo     Python: Full path to Python executable to be used with SNAP, e.g. C:\Python37\python.exe
-@echo     Dir:    Directory where the 'esa_snappy' module should be installed. Defaults to %USERPROFILE%\.snap\snap-python
+@echo     Dir:     Directory where the 'esa_snappy' package should have been installed. Default is the site-packages directory of the Python installation."
 @echo.
 :End
