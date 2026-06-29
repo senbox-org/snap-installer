9ee0f3c046764290eb2b0e3f045626ae6fd8d442
Daniel Nistor <daniel.nistor@cs-soprasteria.com>
2026-06-29 13:35:36 +0300

    update(ci): fix version

--- docker/build/scripts/upload.sh
+++ docker/build/scripts/upload.sh
@@ -12,7 +12,7 @@ do
     then
         echo $file
         export OS=$(echo "$file" | grep -oP  "(linux|windows|macos)")
-        export VERSION="15.0.0-SNAPSHOT"
+        export VERSION="15.0.0"
         export EXT=""
         export GROUP="installers"
         export REPO="snap-maven-releases"
